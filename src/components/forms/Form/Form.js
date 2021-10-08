d38b1632eb243f2bd1a4f8a9780356cf2a5ced42
elvinciqueira <elvinciqueira@gmail.com>
2021-10-08 15:19:34 -0300

    fix: responsive form on mobile

--- src/components/forms/Form/Form.js
+++ src/components/forms/Form/Form.js
@@ -6,10 +6,10 @@ export const Form = ({children, onSubmit, ...props}) => (
     p={4}
     backgroundColor="darkBlue"
     position="absolute"
-    top="20%"
-    left="10%"
+    top={{xs: 0, md: '20%'}}
+    left={{xs: 0, md: '10%'}}
     borderRadius="8px"
-    width="400px"
+    width={{xs: '100%', md: '400px'}}
     {...props}
   >
     <form onSubmit={onSubmit}>{children}</form>
